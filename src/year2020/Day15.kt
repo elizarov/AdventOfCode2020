1484d2e8b3fa370fb7747b2723b7e986fa8eef37
Roman Elizarov <elizarov@gmail.com>
2020-12-15 23:44:30 +0300

    Day15 and some old ones

---
+++ src/year2020/Day15.kt
@@ -0,0 +1,17 @@
+package year2020
+
+fun main() {
+    val nums = "0,3,1,6,7,5".split(",").map { it.toInt() }.toMutableList()
+    val idx = HashMap<Int, Int>()
+    for ((i, last) in nums.dropLast(1).withIndex()) {
+        idx[last] = i
+    }
+    while (nums.size < 30000000) {
+        val last = nums.last()
+        val j = nums.lastIndex
+        val i = idx[last] ?: -1
+        nums.add(if (i < 0) 0 else j - i)
+        idx[last] = j
+    }
+    println(nums.last())
+}
