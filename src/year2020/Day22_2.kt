e3b265d77400fed9c1f32aa907969d2bcf261341
Roman Elizarov <elizarov@gmail.com>
2020-12-26 19:22:02 +0300

    Day 22

--- src/year2020/Day22_2.kt
+++ src/year2020/Day22_2.kt
@@ -15,6 +15,7 @@ fun main() = useFile("src/year2020/Day22.in") {
         val hash = run {
             var res = 0
             for (x in l1) { res = res * 239 + x }
+            res *= 8191
             for (x in l2) { res = res * 239 + x }
             res
         }
@@ -23,14 +24,15 @@ fun main() = useFile("src/year2020/Day22.in") {
         override fun toString(): String = "${l1.contentToString()} -- ${l2.contentToString()}"
     }
     fun cfg(d1: ArrayDeque<Byte>, d2: ArrayDeque<Byte>) = Cfg(d1.toByteArray(), d2.toByteArray())
-    val dp = HashMap<Cfg, Int>()
+    val dp = Array(d1.size + d2.size + 1) { HashMap<Cfg, Int>() }
     var total = 0L
-    fun play(d1: ArrayDeque<Byte>, d2: ArrayDeque<Byte>): Int {
+    fun play(d1: ArrayDeque<Byte>, d2: ArrayDeque<Byte>, level: Int): Int {
+        val d = dp[d1.size + d2.size]
         val cs = HashSet<Cfg>()
         var res = 0
         while (d1.isNotEmpty() && d2.isNotEmpty()) {
             val cfg = cfg(d1, d2)
-            val oldRes = dp[cfg]
+            val oldRes = d[cfg]
             if (oldRes != null) {
                 res = oldRes
                 break
@@ -42,9 +44,12 @@ fun main() = useFile("src/year2020/Day22.in") {
             val f1 = d1.removeFirst()
             val f2 = d2.removeFirst()
             val win = if (f1 <= d1.size && f2 <= d2.size) {
-                val c1 = d1.toCollection(ArrayDeque())
-                val c2 = d2.toCollection(ArrayDeque())
-                play(c1, c2)
+                val c1 = d1.subList(0, f1.toInt()).toCollection(ArrayDeque())
+                val c2 = d2.subList(0, f2.toInt()).toCollection(ArrayDeque())
+                val hr = heuristic(c1, c2)
+                if (hr != 0) hr else {
+                    play(c1, c2, level + 1)
+                }
             } else {
                 if (f1 > f2) 1 else 2
             }
@@ -59,21 +64,25 @@ fun main() = useFile("src/year2020/Day22.in") {
                 }
                 else -> error("cannot be")
             }
+            if (level == 0) {
+                println("[$level] ++ STEP")
+            }
         }
         if (res == 0) res = if (d1.isNotEmpty()) 1 else 2
-        val prevSize = dp.size
-        for (c in cs) dp[c] = res
-        if (dp.size / 10000 != prevSize / 10000) {
-            println("Played ${dp.size} games, last one with ${cs.size} rounds")
-            if (dp.size > 50_000_000) {
-                total += dp.size
-                dp.clear()
-                println("!!!!! CLEAR: TOTAL ROUNDS PLAYED: $total")
+        for (c in cs) d[c] = res
+        val prevSize = total
+        total += cs.size
+        if (total shr 18 != prevSize shr 18) {
+            println("[$level] Played $total rounds; last ${cs.size}; deck ${d1.size + d2.size} cards, win $res")
+            if (d.size > 1_000_000) {
+                d.clear()
+                println("!!!!! CLEARED CURRENT LEVEL CACHE")
             }
         }
         return res
     }
-    val win = play(d1, d2)
+    val win = play(d1, d2, 0)
+    println("Played ${dp.size} rounds")
     println("Winner is $win")
     println("d1 = $d1")
     println("d2 = $d2")
@@ -84,4 +93,11 @@ fun main() = useFile("src/year2020/Day22.in") {
         sum += x * (mul++)
     }
     println(sum)
+}
+
+private fun heuristic(d1: List<Byte>, d2: List<Byte>): Int {
+    val m1 = d1.maxOrNull()!!
+    val m2 = d2.maxOrNull()!!
+    if (m1 > m2 && m1 >= d1.size + d2.size) return 1
+    return 0
 }
